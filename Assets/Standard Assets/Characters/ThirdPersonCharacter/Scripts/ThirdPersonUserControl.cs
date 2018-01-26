988a1c47410a6598b8500a007a459fab5548cde9
wieliczk <wieliczk@ualberta.ca>
2018-01-26 12:36:49 -0700

    Press and Hold E for time slow (There is a cooldown after its used)

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -13,7 +13,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
+		private bool isSlowed = false;
+		private bool slowCooldown = false;
         
+		private float slowTime;
+		private float cooldownTime;
+
         private void Start()
         {
             // get the transform of the main camera
@@ -40,7 +45,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
         }
-
+			
 
         // Fixed update is called in sync with physics
         private void FixedUpdate()
@@ -54,6 +59,75 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 
             bool crouch = Input.GetKey(KeyCode.C);
+
+
+			// I think this shit works
+			// Pretty sure it works correctly
+			// Commented out to try and get a timer working
+			/*
+			if(Input.GetKey(KeyCode.E))
+			{
+				Time.timeScale = 0.5f;
+			} else {
+				Time.timeScale = 1f;
+			}
+			*/
+
+			// If key hit and already not slowed down and not in cool down
+			if (Input.GetKeyDown (KeyCode.E) && !isSlowed && !slowCooldown) 
+			{
+				Debug.Log ("KEY PRESSED");
+				// Do something
+				isSlowed = true;
+				slowCooldown = true;
+				slowTime = 5f;
+				cooldownTime = 5f;
+
+			}
+
+			// If key released and cool down is true
+			if (Input.GetKeyUp (KeyCode.E) && slowCooldown) 
+			{
+				Debug.Log ("KEY RELEASED");
+				isSlowed = false;
+			}
+
+			// Slow time for a period of time  
+			if (isSlowed) 
+			{
+				Debug.Log ("SLOWED");
+				// Timer to slow for a time and end by making isSlowed = true
+				Time.timeScale = 0.5f;
+				slowTime -= Time.deltaTime;
+				//timer(slowTime);
+				//Debug.Log (slowTime);
+				if (slowTime < 0) 
+				{
+					isSlowed = false;
+					//Time.timeScale = 1f;
+					// Reset timer
+					slowTime = 5f;
+				}
+			}
+
+			// no longer slowed, start cooldown 
+			if (!isSlowed && slowCooldown) 
+			{
+				Debug.Log ("COOLDOWN");
+				if (Time.timeScale < 1f) 
+				{
+					Time.timeScale = 1f;
+				}
+				cooldownTime -= Time.deltaTime;
+				Debug.Log (cooldownTime);
+				//timer (cooldownTime);
+				if (cooldownTime < 0) 
+				{
+					slowCooldown = false;
+					cooldownTime = 5f;
+				}
+			}
+					
 
             // calculate move direction to pass to character
             if (m_Cam != null)
