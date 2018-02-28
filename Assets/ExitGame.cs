7c7ee0c7f6497f880879ac36bc5a5c26d34460d7
wieliczk <wieliczk@ualberta.ca>
2018-02-28 02:30:40 -0700

    Simple Main Menu

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour {
+
+	public Button button;
+	// Use this for initialization
+	void Start () 
+	{
+		
+		Button btn = button.GetComponent<Button> ();
+		btn.onClick.AddListener (ifClicked);
+	}
+	
+	void ifClicked()
+	{
+		//Debug.Log ("Clicked");
+		Application.Quit();
+	}
+}
