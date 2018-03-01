d968aec788896b1e223e5eea6b229c658a6404f4
wieliczk <wieliczk@ualberta.ca>
2018-02-28 18:37:40 -0700

    Nothing is working idk whats going on

--- Assets/PausingScript.cs
+++ Assets/PausingScript.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PausingScript : MonoBehaviour {
 
+    [SerializeField] GameObject player;
 	[SerializeField] Camera mainCam;
 	[SerializeField] Camera pauseCam;
 	private bool isPaused = false;
+    MonoBehaviour unit;
 
 
 	// Use this for initialization
 	void Start () 
 	{
+        unit = player.GetComponent("GrapplingHook") as MonoBehaviour;
 		mainCam.GetComponent<Camera> ();
 		mainCam.enabled = true;
 		pauseCam.enabled = false;
@@ -23,23 +26,27 @@ public class PausingScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Escape)) 
 		{
 			if (!isPaused) {
-//				Debug.Log ("NOW PAUSED");
-//				isPaused = true;
-//				Time.timeScale = 0f;
-//				mainCam.enabled = false;
-//				pauseCam.enabled = true;
+                //				Debug.Log ("NOW PAUSED");
+                //				isPaused = true;
+                //				Time.timeScale = 0f;
+                //				mainCam.enabled = false;
+                //				pauseCam.enabled = true;
+                //MonoBehaviour unit = (GetComponent("GrapplingHook") as MonoBehaviour);
+                unit.enabled = false;
 				pause();
 			} else {
-//				Debug.Log ("UNPAUSED");
-//				isPaused = false;
-//				Time.timeScale = 1f;
-//				mainCam.enabled = true;
-//				pauseCam.enabled = false;
+                //				Debug.Log ("UNPAUSED");
+                //				isPaused = false;
+                //				Time.timeScale = 1f;
+                //				mainCam.enabled = true;
+                //				pauseCam.enabled = false;
+                unit.enabled = true;
 				unPause();
 			}
 		}
 		if(Time.timeScale == 1f)
 		{
+            //unit.enabled = true;
 			isPaused = false;
 		}
 	}
