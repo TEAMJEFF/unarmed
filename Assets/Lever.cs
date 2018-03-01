af58702011702e5a634a0e82e5025121190c57d2
iali17 <iali1@ualberta.ca>
2018-03-01 00:21:44 -0700

    YES IT WORKS OMG :goberserk:

---
+++ Assets/Lever.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour {
+
+	public GameObject wall;
+    public GameObject cam;
+
+	public bool leverPulled = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (leverPulled) {
+            
+		}
+	}
+
+	void PulledDown() {
+		leverPulled = true;
+        //GetComponent<CameraShake>().enabled = true;
+        //cam.GetComponent<ThirdPersonCamera>().enabled = false;
+        //cam.GetComponent<Rigidbody>().AddExplosionForce(500f, transform.position, 1f, 100f);
+    }
+}
