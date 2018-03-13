bdae04830c91222bd734378befe2a5030de1ea63
wieliczk <wieliczk@ualberta.ca>
2018-03-12 22:15:54 -0600

    Restarts level

---
+++ Assets/BoundsHandler.cs
@@ -0,0 +1,53 @@
+﻿// based off of https://gist.github.com/NovaSurfer/5f14e9153e7a2a07d7c5
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BoundsHandler : MonoBehaviour
+{
+
+    ScreenFader fadeScr;
+    GameObject thePlayer;
+    Scene thisScene;
+    public int SceneNumb;
+    public float MAX_X;
+    public float MIN_Y;
+    public float MIN_X;
+    //private float MAX_X = 525f;
+    //private float MIN_X = 510f;
+    //private float MIN_Y = 30f;
+
+
+    void Awake()
+    {
+        fadeScr = GameObject.FindObjectOfType<ScreenFader>();
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        thisScene = SceneManager.GetActiveScene();
+        SceneNumb = thisScene.buildIndex;
+    }
+
+    // Update checks bounds
+    void Update()
+    {
+        //Debug.Log(thePlayer.transform.position.x);
+        if(thePlayer.transform.position.x > MAX_X | thePlayer.transform.position.x < MIN_X)
+        {
+            if(thePlayer.transform.position.y < MIN_Y)
+            {
+                fadeScr.BoundsRestart(SceneNumb);
+                //Debug.Log("IN RESTART ZONE");
+            }
+        }
+    }
+   /*
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            fadeScr.EndScene(SceneNumb);
+        }
+    }
+    */
+
+    
+}
---
+++ Assets/ScreenFader.cs
@@ -0,0 +1,120 @@
+﻿// Based off of https://gist.github.com/NovaSurfer/5f14e9153e7a2a07d7c5
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+    public Image FadeImg;
+    public float fadeSpeed = 1.5f;
+    public bool sceneStarting = true;
+
+
+    void Awake()
+    {
+        FadeImg.rectTransform.localScale = new Vector2(Screen.width, Screen.height);
+        FadeImg.enabled = true;
+    }
+
+    //void OnStart()
+
+    void Update()
+    {
+        // If the scene is starting...
+        if (sceneStarting)
+            // ... call the StartScene function.
+            StartScene();
+    }
+
+
+    void FadeToClear()
+    {
+        // Lerp the colour of the image between itself and transparent.
+        FadeImg.color = Color.Lerp(FadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+
+    void FadeToBlack()
+    {
+
+        Debug.Log("THIS HAPPENS");
+        // Lerp the colour of the image between itself and black.
+        FadeImg.color = Color.Lerp(FadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+
+    void StartScene()
+    {
+        // Fade the texture to clear.
+        FadeToClear();
+
+        // If the texture is almost clear...
+        if (FadeImg.color.a <= 0.05f)
+        {
+            // ... set the colour to clear and disable the RawImage.
+            FadeImg.color = Color.clear;
+            FadeImg.enabled = false;
+
+            // The scene is no longer starting.
+            sceneStarting = false;
+        }
+    }
+
+    public IEnumerator BoundsRestartRoutine(int SceneNumber)
+    {
+        //Debug.Log("HERE");
+        FadeImg.enabled = true;
+        //Time.timeScale = 0f;
+        do
+        {
+            FadeToBlack();
+
+            if (FadeImg.color.a >= 0.95f)
+            {
+                SceneManager.LoadScene(SceneNumber);
+                yield break;
+            }
+            else
+            {
+                yield return null;
+            }
+        } while (true);
+    }
+
+    public void BoundsRestart(int SceneNumber)
+    {
+        sceneStarting = false;
+        StartCoroutine("BoundsRestartRoutine", SceneNumber);
+    }
+
+
+    public IEnumerator EndSceneRoutine(int SceneNumber)
+    {
+        // Make sure the RawImage is enabled.
+        FadeImg.enabled = true;
+        do
+        {
+            // Start fading towards black.
+            FadeToBlack();
+
+            // If the screen is almost black...
+            if (FadeImg.color.a >= 0.95f)
+            {
+                // ... reload the level
+                SceneManager.LoadScene(SceneNumber);
+                yield break;
+            }
+            else
+            {
+                yield return null;
+            }
+        } while (true);
+    }
+
+    public void EndScene(int SceneNumber)
+    {
+        sceneStarting = false;
+        StartCoroutine("EndSceneRoutine", SceneNumber);
+    }
+}
