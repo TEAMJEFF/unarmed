ba525bc90c0f271183a87e7318b0f500e0911e27
kierandowns <kdowns@ualberta.ca>
2018-03-22 23:09:12 -0600

    Began implementation
    Rotates chest, doesn't stay and slows model

---
+++ Assets/Scripts/BodyAiming.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class BodyAiming : MonoBehaviour
+{
+		public Transform target;
+
+		void Update () {
+         float mouseInput = Input.GetAxis("Mouse X");
+         Vector3 lookhere = new Vector3(0,mouseInput,0);
+         transform.Rotate(lookhere);
+     }
+}
