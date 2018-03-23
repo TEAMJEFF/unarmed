c29651a0303857c3d893df2edcebbf0ff83cb527
kierandowns <kdowns@ualberta.ca>
2018-03-22 23:58:25 -0600

    Better but
    Please help this is hell

--- Assets/Scripts/BodyAiming.cs
+++ Assets/Scripts/BodyAiming.cs
@@ -7,7 +7,7 @@ public class BodyAiming : MonoBehaviour
 
 		void Update () {
          float mouseInput = Input.GetAxis("Mouse X");
-         Vector3 lookhere = new Vector3(0,mouseInput,0);
+         Vector3 lookhere = new Vector3(0, mouseInput,  0);
          transform.Rotate(lookhere);
      }
 }
