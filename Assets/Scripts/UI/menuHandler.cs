f2af346f3de933ed0256e3fb9c98f0b734b39ad7
wieliczk <wieliczk@ualberta.ca>
2018-04-07 18:42:56 -0600

    fu sound

---
+++ Assets/Scripts/UI/menuHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject mainButtons;
+	public GameObject optionButtons;
+
+	public Button optionButton;
+	public Button goBack;
+
+	void Start () 
+	{
+		optionButton.onClick.AddListener (optionClick);
+		goBack.onClick.AddListener (backClick);
+	}
+
+	void optionClick()
+	{
+		mainButtons.SetActive (false);
+		optionButtons.SetActive (true);
+	}
+
+	void backClick()
+	{
+		optionButtons.SetActive (false);
+		mainButtons.SetActive (true);
+	}
+}
