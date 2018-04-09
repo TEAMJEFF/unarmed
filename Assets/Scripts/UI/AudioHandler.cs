0eef4fb23e373927107a094e3afa442f704d66a0
wieliczk <wieliczk@ualberta.ca>
2018-04-08 18:20:03 -0600

    Sound sliders and stuff work on main menu

---
+++ Assets/Scripts/UI/AudioHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour {
+
+	public AudioSource bgm;
+	private List<AudioSource> sfx;
+	private AudioSource[] initalsound;
+	private GameObject[] gameObjects;
+	private float bgmVolume;
+	private float sfxVolume;
+
+	void Start()
+	{
+
+		if (PlayerPrefs.GetFloat ("bgmVolume") == 0) 
+		{
+			PlayerPrefs.SetFloat ("bgmVolume", 100f);
+			PlayerPrefs.Save ();
+		}
+		if (PlayerPrefs.GetFloat ("sfxVolume") == 0) 
+		{
+			PlayerPrefs.SetFloat ("sfxVolume", 100f);
+			PlayerPrefs.Save ();
+		}
+	}
+
+	// Get all audio sources
+	// Holy shit it gets ALL audio sources...
+	void Awake () 
+	{
+		//Debug.Log (bgm.name);
+		sfx = new List<AudioSource> ();
+		gameObjects = FindObjectsOfType<GameObject> ();
+
+		for (int i = 0; i < gameObjects.Length; i++) 
+		{
+			if (gameObjects [i].GetComponents<AudioSource> () != null) 
+			{
+				initalsound = gameObjects [i].GetComponents<AudioSource> ();
+				//Debug.Log (initalsound);
+				if (initalsound != null) 
+				{
+					//Debug.Log (initalsound.Length);
+					for (int x = 0; x < initalsound.Length; x++) 
+					{
+						if (initalsound [x].name != bgm.name) 
+						{
+							sfx.Add (initalsound [i]);
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+
+	public void UpdateVolumes()
+	{
+		bgmVolume = PlayerPrefs.GetFloat ("bgmVolume");
+		sfxVolume = PlayerPrefs.GetFloat ("sfxVolume");
+		bgm.volume = bgmVolume;
+		for (int i = 0; i < sfx.Count; i++) 
+		{
+			sfx [i].volume = sfxVolume;
+		}
+	}
+
+	public void UpdateBgm()
+	{
+		bgmVolume = PlayerPrefs.GetFloat ("bgmVolume");
+		bgm.volume = bgmVolume;
+	}
+}
--- Assets/Scripts/UI/menuHandler.cs
+++ Assets/Scripts/UI/menuHandler.cs
@@ -12,15 +12,24 @@ public class menuHandler : MonoBehaviour {
 	public Button optionButton;
 	public Button goBack;
 
+	public Slider bgmSliderVol;
+	public Slider sfxSliderVol;
+
+	public AudioHandler audioHandler;
+
 	void Start () 
 	{
 		optionButton.onClick.AddListener (optionClick);
 		goBack.onClick.AddListener (backClick);
+		bgmSliderVol.onValueChanged.AddListener (delegate {bgmVolumeChange();});
+		sfxSliderVol.onValueChanged.AddListener (delegate {sfxVolumeChange();});
+
 	}
 
 	void optionClick()
 	{
 		mainButtons.SetActive (false);
+		updateSliders ();
 		optionButtons.SetActive (true);
 	}
 
@@ -28,5 +37,24 @@ public class menuHandler : MonoBehaviour {
 	{
 		optionButtons.SetActive (false);
 		mainButtons.SetActive (true);
+	}
+
+	void updateSliders()
+	{
+		bgmSliderVol.value = PlayerPrefs.GetFloat ("bgmVolume");
+		sfxSliderVol.value = PlayerPrefs.GetFloat ("sfxVolume");
+	}
+
+	void bgmVolumeChange()
+	{
+		PlayerPrefs.SetFloat ("bgmVolume", bgmSliderVol.value);
+		audioHandler.UpdateBgm ();
+		PlayerPrefs.Save ();
+	}
+
+	void sfxVolumeChange()
+	{
+		PlayerPrefs.SetFloat ("sfxVolume", sfxSliderVol.value);
+		PlayerPrefs.Save ();
 	}
 }
