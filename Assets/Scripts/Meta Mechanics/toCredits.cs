edd6eab5ff73d7d426f8aac14090789a466af000
Imran Ali <iali1@ualberta.ca>
2018-04-10 21:49:55 -0600

    WHAT THE FUCK GITHUB

---
+++ Assets/Scripts/Meta Mechanics/toCredits.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toCredits : MonoBehaviour
+{
+    public ScreenFader fdr;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("In script");
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.tag == "Finish")
+        {
+            fdr.EndScene(3);
+        }
+    }
+}
